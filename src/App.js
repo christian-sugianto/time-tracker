f4da5ba7e19de3e8d58bd66c9042509901681bbc
csugianto <csugianto@student.unimelb.edu.au>
2019-07-05 12:15:31 +1000

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+class App extends Component {
+  state = {
+
+  }
+
+  render () {
+    return (
+      <div className="App">
+        hello world
+      </div>
+    );
+  }
 }
 
 export default App;
