91fa0f6daea2a93e4606688408231bb772c9e3b8
csugianto <csugianto@student.unimelb.edu.au>
2019-07-05 18:22:59 +1000

    add icon, format nav-brand, and add background color to navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ClockPicture from '../../assets/clock.png'
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-color">
+        <a class="navbar-brand" id="navbar-brand-override" href="#">
             FocusTimer
+            <img class="img-fluid" id="img-override" src= { ClockPicture } alt="clock"></img>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -13,19 +15,19 @@ function Navbar() {
 
         <ul class="navbar-nav mr-auto">
             <li class= "nav-item">
-                <a class="nav-link" id="nav-link-overrides" href='#'>About</a>
+                <a class="nav-link" id="nav-link-override" href='#'>About</a>
             </li>
             <li class= "nav-item">
-                <a class="nav-link" id="nav-link-overrides" href='#'>Settings</a>
+                <a class="nav-link" id="nav-link-override" href='#'>Settings</a>
             </li>
         </ul>
 
         <ul class="navbar-nav ml-auto">
             <li class= "nav-item">
-                <a class="nav-link" id="nav-link-overrides" href='#'>Login</a>
+                <a class="nav-link" id="nav-link-override" href='#'>Login</a>
             </li>
             <li class= "nav-item">
-                <a class="nav-link" id="nav-link-overrides" href='#'>Register</a>
+                <a class="nav-link" id="nav-link-override" href='#'>Register</a>
             </li>
         </ul> 
         
