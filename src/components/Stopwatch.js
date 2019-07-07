b8bfee722f248ae6594c91ff3540b9ab368ec4dc
csugianto <csugianto@student.unimelb.edu.au>
2019-07-07 12:51:21 +1000

    update functionality of stopwatch

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -20,48 +20,47 @@ class Stopwatch extends Component {
             <div className="Stopwatch-display">
                 {hours} : {minutes} : {seconds} : {centiseconds}
             </div>
-
-            {this.state.timerOn === false && this.state.timerTime === 0 && (
-            <button onClick={this.startTimer}>Start</button>
-            )}
-            {this.state.timerOn === true && (
-            <button onClick={this.stopTimer}>Stop</button>
-            )}
-            {this.state.timerOn === false && this.state.timerTime > 0 && (
-            <button onClick={this.startTimer}>Resume</button>
-            )}
-            {this.state.timerOn === false && this.state.timerTime > 0 && (
+            
+            <button onClick={this.stopTimer}>Pause</button>
+            <button onClick={this.startTimer}>Play</button>
             <button onClick={this.resetTimer}>Reset</button>
-            )}
+            
         </div>
         );
     }
     
     startTimer = () => {
-        this.setState({
-            timerOn: true,
-            timerTime: this.state.timerTime,
-            timerStart: Date.now() - this.state.timerTime
-        });
-    
-        this.timer = setInterval(() => {
+        if (this.state.timerOn == false) {
             this.setState({
-                timerTime: Date.now() - this.state.timerStart
+                timerOn: true,
+                timerTime: this.state.timerTime,
+                timerStart: Date.now() - this.state.timerTime
             });
-        }, 10);
+        
+            this.timer = setInterval(() => {
+                this.setState({
+                    timerTime: Date.now() - this.state.timerStart
+                });
+            }, 10);
+        }
     };
     
     stopTimer = () => {
-        this.setState({ timerOn: false });
+        this.setState({
+            timerOn: false,
+        })
         clearInterval(this.timer);
-    };
-    
+    }
+
     resetTimer = () => {
         this.setState({
+            timerOn: false,
             timerStart: 0,
             timerTime: 0
-        });
-    };
+        })
+    }
+    
+    
 }
 
 export default Stopwatch;
