98019308396a2941763da15ab020e16a59b2896d
csugianto <csugianto@student.unimelb.edu.au>
2019-07-20 16:37:34 +1000

    remove redux

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,5 @@ import App from './App';
 // import dependencies
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
