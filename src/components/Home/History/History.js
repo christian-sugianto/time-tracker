6780d3bbc57144dd425049583ec99a2cd0070dab
csugianto <csugianto@student.unimelb.edu.au>
2020-02-05 21:38:18 +1100

    update scrollbar

--- src/components/Home/History/History.js
+++ src/components/Home/History/History.js
@@ -1,70 +1,9 @@
 import React, { Component } from "react";
-import {
-  Link,
-  DirectLink,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller
-} from "react-scroll";
 import Record from "./Record";
 
 // components
 
 class History extends Component {
-  constructor(props) {
-    super(props);
-    this.scrollToTop = this.scrollToTop.bind(this);
-  }
-
-  componentDidMount() {
-    Events.scrollEvent.register("begin", function () {
-      console.log("begin", arguments);
-    });
-
-    Events.scrollEvent.register("end", function () {
-      console.log("end", arguments);
-    });
-  }
-  scrollToTop() {
-    scroll.scrollToTop();
-  }
-  scrollTo() {
-    scroller.scrollTo("scroll-to-element", {
-      duration: 800,
-      delay: 0,
-      smooth: "easeInOutQuart"
-    });
-  }
-  scrollToWithContainer() {
-    let goToContainer = new Promise((resolve, reject) => {
-      Events.scrollEvent.register("end", () => {
-        resolve();
-        Events.scrollEvent.remove("end");
-      });
-
-      scroller.scrollTo("scroll-container", {
-        duration: 800,
-        delay: 0,
-        smooth: "easeInOutQuart"
-      });
-    });
-
-    goToContainer.then(() =>
-      scroller.scrollTo("scroll-container-second-element", {
-        duration: 800,
-        delay: 0,
-        smooth: "easeInOutQuart",
-        containerId: "scroll-container"
-      })
-    );
-  }
-  componentWillUnmount() {
-    Events.scrollEvent.remove("begin");
-    Events.scrollEvent.remove("end");
-  }
-
   render() {
     const startTime = new Date(0, 0, 0, 10, 25, 0);
     const endTime = new Date(0, 0, 0, 11, 0, 0);
@@ -77,29 +16,38 @@ class History extends Component {
           <h2 style={{ padding: "10px 10px" }}> History </h2>
         </div>
 
-        <Element
-          name="test7"
-          className="element"
-          id="containerElement"
-          style={{
-            position: "relative",
-            height: "331px",
-            overflow: "scroll",
-            marginBottom: "100px"
-          }}
-        >
-          <div>
-            <Record
-              desc="Studying Computer Systems"
-              startTime={startTime}
-              endTime={endTime}
-            />
-          </div>
-
-          <Element>
-            <Record desc="Pooping" startTime={startTime} endTime={endTime} />
-          </Element>
-        </Element>
+        <div className="scroll">
+          <Record
+            desc="Studying Computer Systems"
+            startTime={startTime}
+            endTime={endTime}
+          />
+          <Record desc="Pooping" startTime={startTime} endTime={endTime} />
+          <Record
+            desc="Studying Computer Systems"
+            startTime={startTime}
+            endTime={endTime}
+          />
+          <Record desc="Pooping" startTime={startTime} endTime={endTime} />
+          <Record
+            desc="Studying Computer Systems"
+            startTime={startTime}
+            endTime={endTime}
+          />
+          <Record desc="Pooping" startTime={startTime} endTime={endTime} />
+          <Record
+            desc="Studying Computer Systems"
+            startTime={startTime}
+            endTime={endTime}
+          />
+          <Record desc="Pooping" startTime={startTime} endTime={endTime} />
+          <Record
+            desc="Studying Computer Systems"
+            startTime={startTime}
+            endTime={endTime}
+          />
+          <Record desc="Pooping" startTime={startTime} endTime={endTime} />
+        </div>
       </div>
     );
   }
